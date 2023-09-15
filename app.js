31cb735fa7f1c8e816debcbd29e5f5d2787a2bc1
Pablo Eduardo CAzon <pabloedu879@gmail.com>
2023-09-15 01:33:13 -0300

    se agrego documentacion de swagger

--- app.js
+++ app.js
@@ -1,9 +1,33 @@
 const express = require("express");
 const cors = require("cors");
+const swaggerJsDoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
 require("dotenv").config();
 const { dbConnection } = require("./src/database/config");
 
 const app = express();
+
+// Opciones de Swagger
+const swaggerOptions = {
+  swaggerDefinition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Mi API",
+      version: "1.0.0",
+      description: "Documentación de mi API",
+      contact: {
+        name: "Rodrigo"
+      },
+      servers: ["http://localhost:4000"]
+    }
+  },
+  // Ubicación de los archivos de rutas para la documentación
+  apis: ["./src/routes/*.js"]
+};
+
+// Inicialización de Swagger
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 // Middlewares
 app.use(express.json()); // Para parsear JSON
@@ -12,7 +36,10 @@ app.use(cors()); // Para habilitar CORS
 // Conexión a la base de datos
 dbConnection()
   .then(() => console.log("Conexión a la base de datos exitosa"))
-  .catch((err) => console.log("Error en la conexión a la base de datos:", err));
+  .catch((err) => {
+    console.error("Error en la conexión a la base de datos:", err);
+    process.exit(1); // Cierra el proceso si la conexión falla
+  });
 
 // Importar rutas
 const authRoutes = require("./src/routes/auth");
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -2,7 +2,65 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
 
+/**
+ * @swagger
+ * /api/users/create:
+ *   post:
+ *     tags:
+ *       - Users
+ *     description: Crea un nuevo usuario
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: body
+ *         description: Datos del usuario
+ *         in: body
+ *         required: true
+ *         schema:
+ *           type: object
+ *           required:
+ *             - name
+ *             - email
+ *             - password
+ *             - rol
+ *           properties:
+ *             name:
+ *               type: string
+ *             email:
+ *               type: string
+ *             password:
+ *               type: string
+ *             rol:
+ *               type: string
+ *     responses:
+ *       201:
+ *         description: Usuario creado exitosamente
+ *       500:
+ *         description: Hubo un error al crear el usuario
+ */
 router.post('/create', userController.createUser);
+
+/**
+ * @swagger
+ * /api/users/deactivate/{id}:
+ *   put:
+ *     tags:
+ *       - Users
+ *     description: Desactiva un usuario
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: id
+ *         description: ID del usuario
+ *         in: path
+ *         required: true
+ *         type: string
+ *     responses:
+ *       200:
+ *         description: Usuario desactivado exitosamente
+ *       500:
+ *         description: Hubo un error al desactivar el usuario
+ */
 router.put('/deactivate/:id', userController.deactivateUser);
 
 module.exports = router;
