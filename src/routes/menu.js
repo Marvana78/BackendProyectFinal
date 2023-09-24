0470d3a191cc15632c7461ba816cf3118b4edb55
LeoDm123 <leo.meiners1@gmail.com>
2023-09-24 19:30:27 -0300

    codigos para editar y borrar productos y combos

--- src/routes/menu.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { AddMenu, GetMenu } = require("../controllers/menu.controllers");
-
-const routerMenu = express.Router();
-
-routerMenu.post("/AddMenu", AddMenu);
-routerMenu.get("/GetMenu", GetMenu);
-
-module.exports = routerMenu;
