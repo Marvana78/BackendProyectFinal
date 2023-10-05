db726b3ff1962efcadddc33eb34f8c867c06e53e
Agüero Hernan <zurdograficos@gmail.com>
2023-10-05 20:19:18 -0300

    si el usuario no existe

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -1,35 +1,6 @@
 const Usuarios = require("../models/usuario-model");
 const bcrypt = require("bcrypt");
 
-const crearUsuario = async (req, res) => {
-  const { name, email, password } = req.body;
-
-  try {
-    //validar si el email del usuario existe en la base de datos
-    let usuario = await Usuarios.findOne({ email });
-    console.log(usuario);
-
-    if (usuario) {
-      return res.json({
-        msg: "El email que intenta registrase ya existe",
-      });
-    }
-
-    usuario = new Usuarios(req.body);
-
-    const salt = bcrypt.genSaltSync(10);
-    usuario.password = bcrypt.hashSync(password, salt);
-
-    await usuario.save();
-
-    res.json({
-      msg: "Usuario Registrado",
-    });
-  } catch (error) {
-    console.log(error);
-  }
-};
-
 const loginUsuario = async (req, res) => {
 	try {
 		const { email, password } = req.body;
